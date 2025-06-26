c29903bf5562ff44aad7f787f8d874b764bf8cd6
Sumanth Kolachalam <kolachalamsumanthbel@gmail.com>
2025-06-26 09:12:40 +0530

    chore: react error boundary

--- apps/pf-ui-host/src/App.tsx
+++ apps/pf-ui-host/src/App.tsx
@@ -1,10 +1,13 @@
 import React, { Suspense, lazy } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 
 const App = () => {
   return (
-    <Suspense fallback={<div>Loading remote component...</div>}>
-      <p>Hi</p>
-    </Suspense>
+    <ErrorBoundary fallback={<div>Something went wrong!</div>}>
+      <Suspense fallback={<div>Loading remote component...</div>}>
+        <p>Hi</p>
+      </Suspense>
+    </ErrorBoundary>
   );
 };
 
